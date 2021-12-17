b1fe9b3e15d1956f7a69cb77056ce3624f20bfc3
Matt Collins <matt@mjolka.com>
2021-12-17 23:15:40 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace dotnet_tcpdump
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var handler = new HttpClientHandler { };
+
+            var client = new HttpClient(handler);
+
+            await client.GetAsync(args[0]);
+        }
+    }
+}
